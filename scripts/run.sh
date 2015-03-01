05f80e96252151619912b2d977e595dcb499f657
Thang Luong <lmthang@stanford.edu>
2015-02-28 16:42:27 -0800

    minor fix to run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -54,7 +54,7 @@ echo "# Script dir = $DIR"
 echo "cd $DIR/../code"
 cd $DIR/../code
 
-MATLAB="matlab"
+#MATLAB="matlab"
 echo "$MATLAB -nodesktop -nodisplay -nosplash -r \"$matlabCommand ; exit()\""
 $MATLAB -nodesktop -nodisplay -nosplash -r "$matlabCommand ; exit()"
 
