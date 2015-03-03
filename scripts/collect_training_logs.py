ffdaf853b2b20f7687d1f98001a92a9f529910ee
Thang Luong <lmthang@stanford.edu>
2015-03-03 15:20:26 -0800

    add collect_training_logs.py

---
+++ scripts/collect_training_logs.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# Author: Thang Luong <luong.m.thang@gmail.com>, created on Tue Mar  3 14:41:27 PST 2015
+
+"""
+Module docstrings.
+"""
+
+usage = 'USAGE DESCRIPTION.' 
+
+### Module imports ###
+import sys
+import os
+import argparse # option parsing
+import re # regular expression
+import codecs
+#sys.path.append(os.environ['HOME'] + '/lib/') # add our own libraries
+
+### Global variables ###
+
+
+### Class declarations ###
+
+
+### Function declarations ###
+def process_command_line():
+  """
+  Return a 1-tuple: (args list).
+  `argv` is a list of arguments, or `None` for ``sys.argv[1:]``.
+  """
+  
+  parser = argparse.ArgumentParser(description=usage) # add description
+  # positional arguments
+  parser.add_argument('in_file', metavar='in_file', type=str, help='list of log files') 
+  parser.add_argument('out_dir', metavar='out_dir', type=str, help='output directory') 
+
+  # optional arguments
+  parser.add_argument('-o', '--option', dest='opt', type=int, default=0, help='option (default=0)')
+  
+  args = parser.parse_args()
+  return args
+
+def check_dir(out_file):
+  dir_name = os.path.dirname(out_file)
+
+  if dir_name != '' and os.path.exists(dir_name) == False:
+    sys.stderr.write('! Directory %s doesn\'t exist, creating ...\n' % dir_name)
+    os.makedirs(dir_name)
+
+def clean_line(line):
+  """
+  Strip leading and trailing spaces
+  """
+
+  line = re.sub('(^\s+|\s$)', '', line);
+  return line
+
+def process_files(in_file, out_dir):
+  """
+  Read data from in_file, and output to out_dir
+  """
+
+  sys.stderr.write('# in_file = %s, out_dir = %s\n' % (in_file, out_dir))
+  # input
+  sys.stderr.write('# Input from %s.\n' % (in_file))
+  inf = open(in_file, 'r')
+
+  # output
+  sys.stderr.write('Output to %s\n' % out_dir)
+  #check_dir(out_dir)
+  #ouf = codecs.open(out_file, 'w', 'utf-8')
+
+  line_id = 0
+  sys.stderr.write('# Processing file %s ...\n' % (in_file))
+  pattern = re.compile('save model test perplexity ([\d\.]+) ')
+  eval_pattern = re.compile('# eval (.+), train')
+  results = []
+  for file_name in inf:
+    file_name = clean_line(file_name)
+    log_file = os.path.expanduser(file_name + '/log')
+    best_ppl = ''
+    eval_stats = ''
+    prev_line = ''
+    if os.path.exists(log_file):
+      log_inf = codecs.open(log_file, 'r', 'utf-8')
+      for line in log_inf: 
+        m = re.search(pattern, line)
+        if m != None:
+          best_ppl = m.group(1)
+
+          # eval stats
+          eval_m = re.search(eval_pattern, prev_line)
+          assert eval_m != None
+          eval_stats = eval_m.group(1)
+
+        prev_line = line
+      log_inf.close()
+    
+    results.append(eval_stats)
+    sys.stderr.write('%s  %s\n' % (eval_stats, file_name))
+  
+  sys.stderr.write('%s\n' % '\n'.join(results))
+  inf.close()
+  #ouf.close()
+
+if __name__ == '__main__':
+  args = process_command_line()
+  process_files(args.in_file, args.out_dir)
+
+
