bff8b315a8d6f5e1b395cbf734190d057f7fae88
Thang Luong <lmthang@stanford.edu>
2015-05-07 16:37:27 -0700

    add softmaxLayerForward/Backprop files

---
+++ code/softmaxLayerBackprop.m
@@ -0,0 +1,16 @@
+function [grad_W, inGrad] = softmaxLayerBackprop(W, inVec, probs, scoreIndices)
+%%%
+%
+% Perform softmax backprop.
+%
+% Thang Luong @ 2015, <lmthang@stanford.edu>
+%
+%%%
+  probs(scoreIndices) = probs(scoreIndices) - 1; % minus one at predicted words
+
+  % softmax_h
+  inGrad = W'* probs;
+
+  % W_soft
+  grad_W = probs*inVec';
+end
