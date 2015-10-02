19ee83ec3378543bef074de0b3a4ef8551773c2c
Thang Luong <lmthang@stanford.edu>
2015-10-02 16:04:31 -0700

    remove eval scripts

--- scripts/eval_newstest2015_ende.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-trans_dir=$1
-~/lstm15/scripts/post_process.py ~/deepmt/data/deen/wmt/newstest2015.split.deen.tok.en $trans_dir/translations.txt $trans_dir/translations.txt.align ~/deepmt/data/deen/wmt/wmt-combined.deen.tok.filtered.f5.new.en-de.dict ~/deepmt/data/deen/wmt/newstest2015.deen.tok.de '' -l de -s ~/deepmt/data/deen/wmt/newstest2015-ende-src.en.sgm -t  ~/deepmt/data/deen/wmt/newstest2015-ende-ref.de.sgm 
