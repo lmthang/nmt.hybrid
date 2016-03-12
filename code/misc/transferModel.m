a00e1497506c602e736f94f83612ffffbb875f5e
Thang Luong <lmthang@stanford.edu>
2016-03-12 00:36:04 -0800

    transfer model now transfer char

--- code/misc/transferModel.m
+++ code/misc/transferModel.m
@@ -1,4 +1,4 @@
-function transferModel(modelFile, srcVocabFile_new, tgtVocabFile_new, outModelFile, varargin)
+function transferModel(modelFile, srcVocabFile_new, tgtVocabFile_new, srcCharPrefix_new, tgtCharPrefix_new, outModelFile, varargin)
   addpath(genpath(sprintf('%s/../..', pwd)));
   
   %% Argument Parser
@@ -7,13 +7,15 @@ function transferModel(modelFile, srcVocabFile_new, tgtVocabFile_new, outModelFi
   addRequired(p,'modelFile',@ischar);
   addRequired(p,'srcVocabFile_new',@ischar);
   addRequired(p,'tgtVocabFile_new',@ischar);
+  addRequired(p,'srcCharPrefix_new',@ischar);
+  addRequired(p,'tgtCharPrefix_new',@ischar);
   addRequired(p,'outModelFile',@ischar);
     
   % optional
   addOptional(p,'gpuDevice', 0, @isnumeric); % choose the gpuDevice to use: 0 -- no GPU 
   
   p.KeepUnmatched = true;
-  parse(p, modelFile, srcVocabFile_new, tgtVocabFile_new, outModelFile, varargin{:})
+  parse(p, modelFile, srcVocabFile_new, tgtVocabFile_new, srcCharPrefix_new, tgtCharPrefix_new, outModelFile, varargin{:})
   transferParams = p.Results;
   
   % GPU settings
@@ -36,6 +38,8 @@ function transferModel(modelFile, srcVocabFile_new, tgtVocabFile_new, outModelFi
   modelFile = transferParams.modelFile;
   srcVocabFile_new = transferParams.srcVocabFile_new;
   tgtVocabFile_new = transferParams.tgtVocabFile_new;
+  srcCharPrefix_new = transferParams.srcCharPrefix_new;
+  tgtCharPrefix_new = transferParams.tgtCharPrefix_new;
   outModelFile = transferParams.outModelFile;
   
   %% Transfer
@@ -45,43 +49,75 @@ function transferModel(modelFile, srcVocabFile_new, tgtVocabFile_new, outModelFi
   model = savedData.model;
   params = savedData.params;
   
-  % load vocabs
-  [srcVocab_new] = loadVocab(srcVocabFile_new);
-  [tgtVocab_new] = loadVocab(tgtVocabFile_new);
+  % src word
+  fprintf(2, '# Transfering src word\n');
+  [model.W_emb_src, params.srcVocab, params.srcVocabFile] = transferEmb(model.W_emb_src, params.srcVocab, params.srcCharShortList, ...
+    srcVocabFile_new, params);
   
-  % prepare new src model
-  fprintf(2, '# Transfering src\n');
-  srcVocabMap = cell2map(params.srcVocab(1:params.srcCharShortList));
-  W_emb_src_new = initMatrixRange(params.initRange, [params.lstmSize, params.srcCharShortList], params.isGPU, params.dataType);
-  srcVocabShortList_new = srcVocab_new(1:params.srcCharShortList);
-  flags = isKey(srcVocabMap, srcVocabShortList_new);
-  indices = values(srcVocabMap, srcVocabShortList_new(flags));
-  indices = [indices{:}];
-  W_emb_src_new(:, flags) = model.W_emb_src(:, indices);
-  model.W_emb_src = W_emb_src_new;
-  params.srcVocab = srcVocab_new;
-  params.srcVocabFile = srcVocabFile_new;
-  fprintf(2, '  src short list %d, num overlap %d\n  new words:', params.srcCharShortList, sum(flags));
-  fprintf(2, ' %s', srcVocabShortList_new{~flags});
-  fprintf(2, '\n');
+  % tgt word
+  fprintf(2, '# Transfering tgt word\n');
+  [model.W_emb_tgt, params.tgtVocab, params.tgtVocabFile] = transferEmb(model.W_emb_tgt, params.tgtVocab, params.tgtCharShortList, ...
+    tgtVocabFile_new, params);
   
-  % prepare new tgt model
-  fprintf(2, '# Transfering tgt\n');
-  tgtVocabMap = cell2map(params.tgtVocab(1:params.tgtCharShortList));
-  W_emb_tgt_new = initMatrixRange(params.initRange, [params.lstmSize, params.tgtCharShortList], params.isGPU, params.dataType);
-  tgtVocabShortList_new = tgtVocab_new(1:params.tgtCharShortList);
-  flags = isKey(tgtVocabMap, tgtVocabShortList_new);
-  indices = values(tgtVocabMap, tgtVocabShortList_new(flags));
-  indices = [indices{:}];
-  W_emb_tgt_new(:, flags) = model.W_emb_tgt(:, indices);
-  model.W_emb_tgt = W_emb_tgt_new;
-  params.tgtVocab = tgtVocab_new;
-  params.tgtVocabFile = tgtVocabFile_new;
-  fprintf(2, '  tgt short list %d, num overlap %d\n  new words:', params.tgtCharShortList, sum(flags));
-  fprintf(2, ' %s', tgtVocabShortList_new{~flags});
-  fprintf(2, '\n');
+  % src char
+  fprintf(2, '# Transfering src char\n');
+  [model.W_emb_src_char, params.srcCharVocab, params.srcCharVocabFile] = transferEmb(model.W_emb_src_char, params.srcCharVocab, ...
+    params.srcCharVocabSize, [srcCharPrefix_new '.char.vocab'], params);
+  params.srcCharMapFile = [srcCharPrefix_new '.char.map'];
+  params.srcCharMap = loadWord2CharMap(params.srcCharMapFile, params.charMaxLen);
+  
+  % tgt char
+  fprintf(2, '# Transfering tgt char\n');
+  [model.W_emb_tgt_char, params.tgtCharVocab, params.tgtCharVocabFile] = transferEmb(model.W_emb_tgt_char, params.tgtCharVocab, ...
+    params.tgtCharVocabSize, [tgtCharPrefix_new '.char.vocab'], params);
+  params.tgtCharMapFile = [tgtCharPrefix_new '.char.map'];
+  params.tgtCharMap = loadWord2CharMap(params.tgtCharMapFile, params.charMaxLen);
   
   % save model
   save(outModelFile, 'model', 'params');
 end
+
+
+function [W_emb_new, vocab_new, vocabFile_new] = transferEmb(W_emb, vocab, shortList, vocabFile_new, params)
+  [vocab_new] = loadVocab(vocabFile_new);
+  vocabMap = cell2map(vocab(1:shortList));
+  W_emb_new = initMatrixRange(params.initRange, [params.lstmSize, shortList], params.isGPU, params.dataType);
+  vocabShortList_new = vocab_new(1:shortList);
+  flags = isKey(vocabMap, vocabShortList_new);
+  indices = values(vocabMap, vocabShortList_new(flags));
+  indices = [indices{:}];
+  W_emb_new(:, flags) = W_emb(:, indices);
+  
+  fprintf(2, '  short list %d, num overlap %d\n  new words:', shortList, sum(flags));
+  fprintf(2, ' %s', vocabShortList_new{~flags});
+  fprintf(2, '\n');
+end
+
+%   srcVocabMap = cell2map(params.srcVocab(1:params.srcCharShortList));
+%   W_emb_src_new = initMatrixRange(params.initRange, [params.lstmSize, params.srcCharShortList], params.isGPU, params.dataType);
+%   srcVocabShortList_new = srcVocab_new(1:params.srcCharShortList);
+%   flags = isKey(srcVocabMap, srcVocabShortList_new);
+%   indices = values(srcVocabMap, srcVocabShortList_new(flags));
+%   indices = [indices{:}];
+%   W_emb_src_new(:, flags) = model.W_emb_src(:, indices);
+%   model.W_emb_src = W_emb_src_new;
+%   params.srcVocab = srcVocab_new;
+%   params.srcVocabFile = srcVocabFile_new;
+%   fprintf(2, '  src short list %d, num overlap %d\n  new words:', params.srcCharShortList, sum(flags));
+%   fprintf(2, ' %s', srcVocabShortList_new{~flags});
+%   fprintf(2, '\n');
+
+%   tgtVocabMap = cell2map(params.tgtVocab(1:params.tgtCharShortList));
+%   W_emb_tgt_new = initMatrixRange(params.initRange, [params.lstmSize, params.tgtCharShortList], params.isGPU, params.dataType);
+%   tgtVocabShortList_new = tgtVocab_new(1:params.tgtCharShortList);
+%   flags = isKey(tgtVocabMap, tgtVocabShortList_new);
+%   indices = values(tgtVocabMap, tgtVocabShortList_new(flags));
+%   indices = [indices{:}];
+%   W_emb_tgt_new(:, flags) = model.W_emb_tgt(:, indices);
+%   model.W_emb_tgt = W_emb_tgt_new;
+%   params.tgtVocab = tgtVocab_new;
+%   params.tgtVocabFile = tgtVocabFile_new;
+%   fprintf(2, '  tgt short list %d, num overlap %d\n  new words:', params.tgtCharShortList, sum(flags));
+%   fprintf(2, ' %s', tgtVocabShortList_new{~flags});
+%   fprintf(2, '\n');
   
--- scripts/transferModel.sh
+++ scripts/transferModel.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-if [[ $# -lt 4 || $# -gt 5 ]]; then
+if [[ $# -lt 6 || $# -gt 7 ]]; then
   echo -e "`basename $0`\tGenerate new model file w.r.t new vocab files"
   echo -e "\tmodelFile\t\tModel file (.mat)."
   echo -e "\tsrcVocabFile\tnew src vocab file."
   echo -e "\ttgtVocabFile\tnew tgt vocab file."
+  echo -e "\tsrcCharPrefix\tnew src char prefix."
+  echo -e "\ttgtCharPrefix\tnew tgt char prefix."
   echo -e "\toutputFile\t\toutput new model file"
   exit
 fi
 
 modelFile=$1
 srcVocabFile=$2
-tgtVocabSize=$3
-outputFile=$4
-basicOpt="'$modelFile','$srcVocabFile','$tgtVocabSize','$outputFile'"
-if [ $# -eq 5 ]; then
-  matlabCommand="transferModel($basicOpt,${5})"
+tgtVocabFile=$3
+srcCharPrefix=$4
+tgtCharPrefix=$5
+outputFile=$6
+basicOpt="'$modelFile','$srcVocabFile','$tgtVocabFile','srcCharPrefix','tgtCharPrefix','$outputFile'"
+if [ $# -eq 7 ]; then
+  matlabCommand="transferModel($basicOpt,${7})"
 else
   matlabCommand="transferModel($basicOpt)"
 fi
